e3125c72ebea4ddbc118a4208a3d6a18c4187560
Sergi Besses <sergi.bema@gmail.com>
2018-12-08 19:13:06 +0100

    upload file

---
+++ change_activities_enum_field.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
+/*
+
+This migration shows how to change an enum field called "activities"
+with the original options: ['debate', 'table', 'update'] to add the 
+following options: ['debate', 'table', 'update', 'session', 'workshop']
+
+*/
+class ChangeActivitiesTypeField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE activities MODIFY COLUMN type ENUM('debate', 'table', 'update', 'session', 'workshop')");
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("ALTER TABLE activities MODIFY COLUMN type ENUM('debate', 'table', 'update')");
+    }
+}
